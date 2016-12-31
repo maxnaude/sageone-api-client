8fdf40beb635c6aa9c7f6333619b209e44edd6bb
Max Naude <maxnaude@gmail.com>
2016-12-31 22:41:29 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sageone-api-client',
+    version='0.0.1',
+    description='Sage One API Client',
+    author='Max Naude',
+    author_email='maxnaude@gmail.com',
+    url='https://github.com/maxnaude/sageone-api-client',
+    packages=find_packages(),
+    dependency_links=[],
+    install_requires=[
+        'hammock==0.2.4'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
